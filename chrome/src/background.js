fd1426f3e3aa5f77dcddbabb8f12a9f422773a5a
Stijn De Clercq <declercq.stijn@outlook.com>
2021-01-03 23:59:47 +0100

    Introduce storage for on/off

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,7 +1,18 @@
 /** @format */
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ active: true }, function () {
-    console.log("Test");
+  // In case the plugin has been installed before, use the value
+  // that was set by the user previously instead of resetting
+  let isActive = true;
+  chrome.storage.sync.get(["active"], function (res) {
+    if (!(Object.keys(res).length === 0)) {
+      isActive = res.active;
+    }
+  });
+
+  // Insert value into storage, nothing has to happen with it
+  // so the function can be empty
+  chrome.storage.sync.set({ active: isActive }, function () {
+    console.log(isActive);
   });
 });
